e92485ba3475396f2ca5910d7a323d86c100d67d
Henrik Melsom <melsom.henrik@gmail.com>
2019-12-21 17:05:11 +0100

    solved day part 2 through brute force

--- 02/2/main.py
+++ 02/2/main.py
@@ -1,34 +1,35 @@
 from os import path
 __dir__ = path.dirname(__file__)
 
-with open(f'{__dir__}/in', 'r') as f:
-    intcode = [int(x) for x in f.read().split(',')]
+for noun in range(0, 99):
+    for verb in range(0, 99):
+        with open(f'{__dir__}/in', 'r') as f:
+            intcode = [int(x) for x in f.read().split(',')]
+        
+        intcode[1] = noun
+        intcode[2] = verb
 
-print(intcode)
+        for i in range(0, len(intcode), 4):
+            opcode = intcode[i]
 
-intcode[1] = 12
-intcode[2] = 2
+            if opcode == 99:
+                break
 
-print(intcode)
+            input1 = intcode[intcode[i+1]]
+            input2 = intcode[intcode[i+2]]
+            output_pos = intcode[i+3]
 
-for i in range(0, len(intcode), 4):
-    opcode = intcode[i]
+            output = 0
+            if opcode == 1:
+                output = input1 + input2
+            elif opcode == 2:
+                output = input1 * input2
+            else:
+                raise RuntimeError(f'encountered opcode: {opcode}')
 
-    if opcode == 99:
-        break
+            intcode[output_pos] = output
 
-    input1 = intcode[intcode[i+1]]
-    input2 = intcode[intcode[i+2]]
-    output_pos = intcode[i+3]
-
-    output = 0
-    if opcode == 1:
-        output = input1 + input2
-    elif opcode == 2:
-        output = input1 * input2
-    else:
-        raise RuntimeError(f'encountered opcode: {opcode}')
-
-    intcode[output_pos] = output
-
-print(intcode)
+        if intcode[0] == 19690720:
+            print(f'{intcode[0]} = f({noun}, {verb})')
+            print(100 * noun + verb)
+            break
