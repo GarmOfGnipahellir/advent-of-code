3c2182892b4de2cfd751c73523071541a9f4fcc8
Henrik Melsom <garm@Henriks-MacBook-Pro.local>
2020-12-14 21:46:26 +0100

    recovered 13 01

--- 2020/13/01.py
+++ 2020/13/01.py
@@ -1,59 +1,22 @@
 from math import inf
-from dataclasses import dataclass
-
-@dataclass
-class Bus:
-    bid: int
-    oid: int
-    step: int = 0
-
-    def get_time(self) -> int:
-        return self.bid * self.step
-
-    def step_to_earliest_time_past(self, t: int) -> int:
-        while self.get_time() < t:
-            self.step += 1
-        return self.get_time()
-
-    def get_time_equality(self) -> int:
-        return self.get_time() - self.oid
 
 inp = open("./in00", mode="r").readlines()
 inp = [line.strip() for line in inp]
 
 earliest_departure = int(inp[0])
-busses = [Bus(int(x), i) for i, x in enumerate(inp[1].split(",")) if x != "x"]
+busses = [int(x) for x in inp[1].split(",") if x != "x"]
 
-for i, b in enumerate(busses):
-    print(i, b)
-
-n = 0
-while True:
-    n += 1
-    if n % 100000 == 0:
-        print(n)
-        for i, b in enumerate(busses):
-            print(i, b)
-    time_equalized = True
-
-    for bus1 in busses:
-        for bus2 in busses:
-            if bus1.get_time_equality() != bus2.get_time_equality():
-                time_equalized = False
-                break
-        if not time_equalized:
-            break
+best_bus = -1
+best_time = inf
+for bus in busses:
+    t = 0
+    while t < earliest_departure:
+        t += bus
     
-    for i in range(len(busses)-1, -1, -1):
-        if i == 0:
-            busses[i].step += 1
-            break
-        elif busses[i].get_time_equality() < busses[i-1].get_time_equality():
-            busses[i].step += 1
-            break
+    if t < best_time:
+        best_time = t
+        best_bus = bus
 
-    if time_equalized:
-        print("time equalized")
-        break
-
-print(busses[0].get_time())
+print(best_bus, best_time)
+print(best_time - earliest_departure)
+print((best_time - earliest_departure) * best_bus)
