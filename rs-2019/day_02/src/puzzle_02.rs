54a0c135ef1452ca48ed295486c97fbb8708a299
Henrik Melsom <melsom.henrik@gmail.com>
2021-08-01 23:08:41 +0200

    d02p02 done

--- rs-2019/day_02/src/puzzle_02.rs
+++ rs-2019/day_02/src/puzzle_02.rs
@@ -1,3 +1,21 @@
+use shared::{
+    intcode::{Computer, Program},
+    read_string,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let contents = read_string("day_02/data/input.txt").unwrap();
+    let program = contents.parse::<Program>().unwrap();
+    for noun in 0..100 {
+        for verb in 0..100 {
+            let mut computer = Computer::new(program.clone());
+            computer.set(1, noun).unwrap();
+            computer.set(2, verb).unwrap();
+            computer.run().unwrap();
+            if computer.get(0).unwrap() == 19690720 {
+                println!("{}", 100 * noun + verb);
+                return;
+            }
+        }
+    }
 }
