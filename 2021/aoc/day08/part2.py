ff78050d29cdc1cd51c2c47fc0d23453e31589cb
Garm <melsom.henrik@gmail.com>
2021-12-10 00:37:54 +0100

    got soem ideas fro 2021 day08

--- 2021/aoc/day08/part2.py
+++ 2021/aoc/day08/part2.py
@@ -1,115 +1,29 @@
 # Advent of Code - Day 8 - Part Two
 
-from typing import List
-
-DIGITS = [
-    "abcefg",
-    "cf",
-    "acdeg",
-    "acdfg",
-    "bcdf",
-    "abdfg",
-    "abdefg",
-    "acf",
-    "abcdefg",
-    "abcdfg",
-]
-
-LENGTHS = {
-    2: [1],
-    3: [7],
-    4: [4],
-    5: [2, 3, 5],
-    6: [0, 6, 9],
-    7: [8],
-}
+from typing_extensions import Self
 
 
-class SignalMap:
-    dict = {
-        "a": list("abcdefg"),
-        "b": list("abcdefg"),
-        "c": list("abcdefg"),
-        "d": list("abcdefg"),
-        "e": list("abcdefg"),
-        "f": list("abcdefg"),
-        "g": list("abcdefg"),
-    }
+class Digit:
+    chars: str
 
-    def __init__(self, inputs: List[str]):
-        self.solve_map(inputs)
+    def __init__(self, chars: str) -> None:
+        self.chars = chars
 
-    def __getitem__(self, key: str) -> List[str]:
-        return self.dict[key]
+    def __str__(self) -> str:
+        return self.chars
 
-    def __setitem__(self, key: str, val: str) -> None:
-        self.dict[key] = val
+    def __eq__(self, other: Self) -> bool:
+        if type(self) != type(other):
+            return False
+        return self.chars == other.chars
 
-    def solve_digit(self, source: str, digit: str) -> None:
-        for k in self.dict.keys():
-            if k in digit:
-                self[k] = list(filter(lambda ch: ch in source, self[k]))
-            else:
-                self[k] = list(filter(lambda ch: ch not in source, self[k]))
-
-    def solve_recursive(self, input: str, indices: List[int]) -> bool:
-        candidates = [DIGITS[i] for i in LENGTHS[len(input)]]
-        mapped = "".join([self[ch][indices[i]] for i, ch in enumerate(input)])
-        if mapped not in candidates:
-            for i in range(len(indices)):
-                if indices[i] < len(list(self.dict.values())[i]) - 1:
-                    indices[i] += 1
-                    return self.solve_recursive(input, indices)
-        return mapped in candidates
-
-    def solve_map(self, inputs: List[str]) -> None:
-        for inp in inputs:
-            l = len(inp)
-            for i in LENGTHS[l]:
-                if l == 2 or l == 3 or l == 4 or l == 7:
-                    self.solve_digit(inp, DIGITS[i])
-
-        unknowns = filter(lambda x: len(x) > 4 and len(x) != 7, inputs)
-        for inp in unknowns:
-            l = len(inp)
-            for i in LENGTHS[l]:
-                indices = [0 for i in range(len(inp))]
-                if self.solve_recursive(inp, indices):
-                    res = [self[ch][indices[i]] for i, ch in enumerate(inp)]
-                    print(res)
-
-        for k, v in self.dict.items():
-            print(k, v)
-        pass
-
-    def map(self, input: str) -> str:
-        return "".join([self[ch] for ch in input])
-
-
-def print_digit(input):
-    print("  {0}{0}{0}{0}  ".format("█" if "a" in input else " "))
-    print("{0}{0}    {1}{1}".format("█" if "b" in input else " ",
-          "█" if "c" in input else " "))
-    print("{0}{0}    {1}{1}".format("█" if "b" in input else " ",
-          "█" if "c" in input else " "))
-    print("  {0}{0}{0}{0}  ".format("█" if "d" in input else " "))
-    print("{0}{0}    {1}{1}".format("█" if "e" in input else " ",
-          "█" if "f" in input else " "))
-    print("{0}{0}    {1}{1}".format("█" if "e" in input else " ",
-          "█" if "f" in input else " "))
-    print("  {0}{0}{0}{0}  ".format("█" if "g" in input else " "))
+    def __sub__(self, other: Self) -> Self:
+        tmp = self.chars
+        for ch in other.chars:
+            tmp = tmp.replace(ch, "")
+        return Digit(tmp)
 
 
 def result(input):
     input = [(*[spl.split() for spl in ln.split("|")],) for ln in input]
-
-    for entry in input:
-        signalmap = SignalMap(entry[0])
-        for inp in entry[0]:
-            mapped = signalmap.map(inp)
-            print(inp)
-            print(mapped)
-            print_digit(mapped)
-        break
-
     return None
--- 2021/tests/test_day08.py
+++ 2021/tests/test_day08.py
@@ -1,5 +1,6 @@
 from aoc.day08 import part1, part2
 from aoc.day08.__main__ import read_file
+from aoc.day08.part2 import Digit
 
 #
 # --- Part One ---
@@ -14,10 +15,14 @@ def test_part1():
 #
 
 
-def test_signal_map():
-    signalmap = part2.SignalMap(
-        ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"])
-    assert "".join([val[0] for val in signalmap.dict.values()]) == "deafgbc"
+# def test_signal_map():
+#     signalmap = part2.SignalMap(
+#         ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"])
+#     assert "".join([val[0] for val in signalmap.dict.values()]) == "deafgbc"
+
+
+def test_digit():
+    assert Digit("dab") - Digit("ab") == Digit("d")
 
 
 # def test_part2():
