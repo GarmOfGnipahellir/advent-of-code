7e2a209d5cc7a4a6f49f2fc2ba063e0cad0b3526
Garm <melsom.henrik@gmail.com>
2021-12-15 09:41:56 +0100

    completed 2021 day14 part1 part2  is an optimization thing again

--- 2021/aoc/day14/part1.py
+++ 2021/aoc/day14/part1.py
@@ -1,4 +1,42 @@
 # Advent of Code - Day 14 - Part One
 
+from typing import Dict, List
+
+
+class Polymerizer:
+    template: str
+    rules: Dict[str, str]
+    polymer: str
+
+    def __init__(self, input: List[str]) -> None:
+        self.template = input[0]
+        self.rules = {}
+        for ln in input[2:]:
+            src, el = ln.split(" -> ")
+            self.rules[src] = el
+        self.polymer = self.template
+
+    def step(self) -> None:
+        buf = list(self.polymer)
+        cur = 1
+        while cur < len(buf):
+            pair = buf[cur-1] + buf[cur]
+            buf.insert(cur, self.rules[pair])
+            cur += 2
+        self.polymer = "".join(buf)
+
+
 def result(input):
-    return None
+    polymerizer = Polymerizer(input)
+    for _ in range(10):
+        polymerizer.step()
+
+    elements = {}
+    for el in polymerizer.polymer:
+        if el not in elements.keys():
+            elements[el] = 0
+        elements[el] += 1
+
+    most = max(elements.values())
+    least = min(elements.values())
+    return most - least
--- 2021/tests/test_day14.py
+++ 2021/tests/test_day14.py
@@ -5,8 +5,20 @@ from aoc.day14.__main__ import read_file
 # --- Part One ---
 #
 
+def test_polymerizer():
+    polymerizer = part1.Polymerizer(read_file("./resources/example.txt"))
+    assert polymerizer.polymer == "NNCB"
+    polymerizer.step()
+    assert polymerizer.polymer == "NCNBCHB"
+    polymerizer.step()
+    assert polymerizer.polymer == "NBCCNBBBCBHCB"
+    polymerizer.step()
+    assert polymerizer.polymer == "NBBBCNCCNBBNBNBBCHBHHBCHB"
+    polymerizer.step()
+    assert polymerizer.polymer == "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
+
 def test_part1():
-    assert part1.result(read_file("./resources/example.txt")) == None
+    assert part1.result(read_file("./resources/example.txt")) == 1588
 
 #
 # --- Part Two ---
