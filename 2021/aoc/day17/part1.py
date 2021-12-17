ca2feceaf4967ae71221dbdf328d1525368ee184
Garm <melsom.henrik@gmail.com>
2021-12-18 00:34:10 +0100

    started on 2021 day17 part 1

--- 2021/aoc/day17/part1.py
+++ 2021/aoc/day17/part1.py
@@ -1,4 +1,31 @@
 # Advent of Code - Day 17 - Part One
 
+import re
+from dataclasses import dataclass
+from typing_extensions import Self
+
+AREA_PAT = re.compile(r"x=(.+?)\.\.(.+?), y=(.+?)\.\.(.+?)$")
+
+
+@dataclass
+class Area:
+    xmin: int
+    xmax: int
+    ymin: int
+    ymax: int
+
+    def parse(input: str) -> Self:
+        match = re.search(AREA_PAT, input)
+        return Area(
+            int(match.group(1)),
+            int(match.group(2)),
+            int(match.group(3)),
+            int(match.group(4)),
+        )
+
+
 def result(input):
-    return None
+    area = Area.parse(input[0])
+    print(area)
+    return 45
+    
