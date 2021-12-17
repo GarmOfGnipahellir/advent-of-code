821c7735a78c234913cf6f8ab6cf7a1dce8e3944
Garm <melsom.henrik@gmail.com>
2021-12-17 01:42:41 +0100

    started on 2021 day16 part1

--- 2021/aoc/day16/part1.py
+++ 2021/aoc/day16/part1.py
@@ -1,4 +1,65 @@
 # Advent of Code - Day 16 - Part One
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Packet:
+    data: str
+    version: int
+    typeid: int
+
+
+@dataclass
+class Literal:
+    version: int
+    value: int
+
+
+@dataclass
+class Operator:
+    version: int
+
+
+def hex_to_bin(input: str) -> str:
+    return f"{int(input, 16):b}".zfill(len(input) * 4)
+
+
+def parse_literal(input: str) -> Literal:
+    version = int(input[:3], 2)
+    b = ""
+    i = 0
+    while True:
+        grp = input[6+5*i:11+5*i]
+        b += grp[1:]
+        if grp[:1] == "0":
+            break
+        i += 1
+    return Literal(version, int(b, 2))
+
+
+def parse_operator(input: str) -> Operator:
+    version = int(input[:3], 2)
+    m = input[6:7]
+    print(m)
+    if m == "0":
+        l = int(input[7:22], 2)
+        print(l)
+    else:
+        pass
+    return Operator(version)
+
+
+def parse(input: str) -> Literal:
+    typeid = int(input[3:6], 2)
+
+    if typeid == 4:
+        return parse_literal(input)
+    else:
+        return parse_operator(input)
+
+
 def result(input):
-    return None
+    b = hex_to_bin(input[0])
+    p = parse(b)
+    return p.version
--- 2021/tests/test_day16.py
+++ 2021/tests/test_day16.py
@@ -5,12 +5,30 @@ from aoc.day16.__main__ import read_file
 # --- Part One ---
 #
 
+
+def test_hex_to_bin():
+    assert part1.hex_to_bin("D2FE28") == "110100101111111000101000"
+    assert part1.hex_to_bin(
+        "38006F45291200") == "00111000000000000110111101000101001010010001001000000000"
+
+
+def test_parse():
+    assert part1.parse(part1.hex_to_bin("D2FE28")) == part1.Literal(6, 2021)
+    assert part1.parse(part1.hex_to_bin("38006F45291200")) == part1.Operator(1)
+
+
 def test_part1():
-    assert part1.result(read_file("./resources/example.txt")) == None
+    assert part1.result(["D2FE28"]) == 6
+    assert part1.result(["38006F45291200"]) == 9
+    assert part1.result(["8A004A801A8002F478"]) == 16
+    assert part1.result(["620080001611562C8802118E34"]) == 12
+    assert part1.result(["C0015000016115A2E0802F182340"]) == 23
+    assert part1.result(["A0016C880162017C3686B18A3D4780"]) == 31
 
 #
 # --- Part Two ---
 #
+
 
 def test_part2():
     assert part2.result(read_file("./resources/example.txt")) == None
