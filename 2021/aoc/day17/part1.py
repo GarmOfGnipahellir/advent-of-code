3e3e0f118ac54dea0c42cfdc4feb6b64a1076bb5
Garm <melsom.henrik@gmail.com>
2021-12-18 22:57:56 +0100

    completed 2021 day17 part1

--- 2021/aoc/day17/part1.py
+++ 2021/aoc/day17/part1.py
@@ -1,6 +1,7 @@
 # Advent of Code - Day 17 - Part One
 
 import re
+import math
 from dataclasses import dataclass
 from typing_extensions import Self
 
@@ -37,8 +38,6 @@ class Area:
         )
 
 
-# x = v*x - 0.5 * max(x-0.5, 0)^2
-# y = v*x - 0.5 * (x-0.5)^2
 @dataclass
 class Probe:
     pos: Vec
@@ -53,12 +52,43 @@ class Probe:
         self.vel.y -= 1
 
 
+@dataclass
+class Trajectory:
+    initial_vel: Vec
+
+    def max_x_time(self) -> int:
+        return abs(self.initial_vel.x)
+
+    def time_at_y(self, y) -> float:
+        v2 = self.initial_vel.y * 2
+        return 0.5 * (math.sqrt((v2 + 1) ** 2 - 8 * y) + v2 + 1)
+
+    def evaluate(self, time: int) -> Vec:
+        xt = min(time, self.max_x_time())
+        return Vec(
+            ((self.initial_vel.x * 2 + 1) * xt - xt ** 2) / 2,
+            ((self.initial_vel.y * 2 + 1) * time - time ** 2) / 2,
+        )
+
+
 def result(input):
     area = Area.parse(input[0])
-    print(area)
-    probe = Probe(Vec(0, 0), Vec(6, 3))
-    print(probe)
-    for _ in range(7):
-        probe.step()
-        print(probe)
-    return 45
+    record_vy = 0
+    record_t = 0
+    t = 0
+    for vy in range(0, abs(area.max.y) * 10):
+        hit = False
+        trajectory = Trajectory(Vec(0, vy))
+        for y in reversed(range(area.min.y, area.max.y + 1)):
+            t = trajectory.time_at_y(y)
+            if round(t) == t:
+                hit = True
+                break
+
+        if hit:
+            record_vy = vy
+            record_t = t
+
+        vy += 1
+
+    return Trajectory(Vec(0, record_vy)).evaluate(record_t // 2).y
--- 2021/tests/test_day17.py
+++ 2021/tests/test_day17.py
@@ -1,20 +1,46 @@
 from aoc.day17 import part1, part2
 from aoc.day17.__main__ import read_file
-from aoc.day17.part1 import Vec
+from aoc.day17.part1 import Probe, Vec, Trajectory
 
 #
 # --- Part One ---
 #
 
+
 def test_vec():
     assert Vec(1, 2) + Vec(3, 4) == Vec(4, 6)
 
+
+def test_trajectory():
+    probe = Probe(Vec(0, 0), Vec(7, 2))
+    trajectory = Trajectory(Vec(7, 2))
+    assert trajectory.time_at_y(-7) == 7
+    for i in range(100):
+        probe.step()
+        assert trajectory.evaluate(i+1) == probe.pos
+    
+    probe = Probe(Vec(0, 0), Vec(6, 3))
+    trajectory = Trajectory(Vec(6, 3))
+    assert trajectory.time_at_y(-9) == 9
+    for i in range(100):
+        probe.step()
+        assert trajectory.evaluate(i+1) == probe.pos
+    
+    probe = Probe(Vec(0, 0), Vec(9, 0))
+    trajectory = Trajectory(Vec(9, 0))
+    assert trajectory.time_at_y(-10) == 5
+    for i in range(100):
+        probe.step()
+        assert trajectory.evaluate(i+1) == probe.pos
+
+
 def test_part1():
-    assert part1.result(read_file("./resources/example.txt")) == None
+    assert part1.result(read_file("./resources/example.txt")) == 45
 
 #
 # --- Part Two ---
 #
+
 
 def test_part2():
     assert part2.result(read_file("./resources/example.txt")) == None
