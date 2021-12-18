4253f8ac651f121f4486045c46d83dae2b0c8669
Garm <melsom.henrik@gmail.com>
2021-12-18 11:40:02 +0100

    continued on 2021 day 17 part1

--- 2021/aoc/day17/part1.py
+++ 2021/aoc/day17/part1.py
@@ -8,24 +8,57 @@ AREA_PAT = re.compile(r"x=(.+?)\.\.(.+?), y=(.+?)\.\.(.+?)$")
 
 
 @dataclass
+class Vec:
+    x: int
+    y: int
+
+    def __add__(self, other: object) -> Self:
+        if type(self) != type(other):
+            raise ValueError(f"cant add Vec with {type(other)}")
+        return Vec(self.x + other.x, self.y + other.y)
+
+
+@dataclass
 class Area:
-    xmin: int
-    xmax: int
-    ymin: int
-    ymax: int
+    min: Vec
+    max: Vec
 
     def parse(input: str) -> Self:
         match = re.search(AREA_PAT, input)
         return Area(
-            int(match.group(1)),
-            int(match.group(2)),
-            int(match.group(3)),
-            int(match.group(4)),
+            Vec(
+                int(match.group(1)),
+                int(match.group(3)),
+            ),
+            Vec(
+                int(match.group(2)),
+                int(match.group(4)),
+            ),
         )
+
+
+# x = v*x - 0.5 * max(x-0.5, 0)^2
+# y = v*x - 0.5 * (x-0.5)^2
+@dataclass
+class Probe:
+    pos: Vec
+    vel: Vec
+
+    def step(self) -> None:
+        self.pos += self.vel
+        if self.vel.x > 0:
+            self.vel.x -= 1
+        elif self.vel.x < 0:
+            self.vel.x += 1
+        self.vel.y -= 1
 
 
 def result(input):
     area = Area.parse(input[0])
     print(area)
+    probe = Probe(Vec(0, 0), Vec(6, 3))
+    print(probe)
+    for _ in range(7):
+        probe.step()
+        print(probe)
     return 45
-    
--- 2021/tests/test_day17.py
+++ 2021/tests/test_day17.py
@@ -1,9 +1,13 @@
 from aoc.day17 import part1, part2
 from aoc.day17.__main__ import read_file
+from aoc.day17.part1 import Vec
 
 #
 # --- Part One ---
 #
+
+def test_vec():
+    assert Vec(1, 2) + Vec(3, 4) == Vec(4, 6)
 
 def test_part1():
     assert part1.result(read_file("./resources/example.txt")) == None
