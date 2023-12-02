2f018bf79780f234142784128760863e8f4b6687
Garm <melsom.henrik@gmail.com>
2023-12-02 13:36:59 +0100

    2023 day 01 part 2 done

--- 2023/01/__init__.py
+++ 2023/01/__init__.py
@@ -1,6 +1,7 @@
 """https://adventofcode.com/2023/day/1"""
 
 from os import path
+from typing import Optional
 
 EX1 = """1abc2
 pqr3stu8vwx
@@ -15,6 +16,21 @@ xtwone3four
 zoneight234
 7pqrstsixteen"""
 
+DIGITS = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+
+# ---------------------------------- PART 1 ---------------------------------- #
+
 
 def part1(inp: str):
     values = []
@@ -28,17 +44,43 @@ def part1(inp: str):
     return sum(values)
 
 
-# TODO: Custom parser
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------- PART 2 ---------------------------------- #
+
+
+def parse_digit(inp: str, start: int) -> Optional[str]:
+    if inp[start].isdigit():
+        return inp[start]
+
+    buffer = ""
+    for i in range(start, len(inp)):
+        if inp[i].isdigit():
+            break
+
+        buffer += inp[i]
+
+        if buffer in DIGITS.keys():
+            return DIGITS[buffer]
+
+    return None
+
+
 def part2(inp: str):
     values = []
     for line in inp.splitlines():
         digits = ""
-        for ch in line:
-            if ch.isdigit():
-                digits += ch
+        for i in range(len(line)):
+            digit = parse_digit(line, i)
+            if digit:
+                digits += digit
         value = digits[0] + digits[-1]
         values.append(int(value))
     return sum(values)
+
+
+# ---------------------------------------------------------------------------- #
 
 
 if __name__ == "__main__":
