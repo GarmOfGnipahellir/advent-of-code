8e5ea8ba2102544560c54c655bb96dc149ccc25a
Garm <melsom.henrik@gmail.com>
2023-12-03 01:00:26 +0100

    2023 day 02 part 1 done

--- 2023/02/__init__.py
+++ 2023/02/__init__.py
@@ -2,7 +2,8 @@
 
 from dataclasses import dataclass
 from os import path
-from typing import Optional
+from typing import Optional, List
+import re
 
 EX1 = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -14,20 +15,62 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 # ---------------------------------- PART 1 ---------------------------------- #
 
 
+HAND_PAT = re.compile(r"(\d+) ((?:red)|(?:green)|(?:blue))")
+
+
+@dataclass
+class Bag:
+    red: int
+    green: int
+    blue: int
+
+
+@dataclass
+class Hand:
+    red: int = 0
+    green: int = 0
+    blue: int = 0
+
+    @staticmethod
+    def parse(inp: str):
+        args = {}
+        for m in HAND_PAT.finditer(inp):
+            num = int(m.group(1))
+            col = m.group(2)
+            args[col] = num
+        return Hand(**args)
+
+    def ispossible(self, bag: Bag):
+        return self.red <= bag.red and self.green <= bag.green and self.blue <= bag.blue
+
+
 @dataclass
 class Game:
     id: int
-    red: int
-    blue: int
-    green: int
+    hands: List[Hand]
 
     @staticmethod
     def parse(inp: str):
         [head, tail] = inp.split(":")
+        id = int(head.replace("Game ", ""))
+        hands = [Hand.parse(inp) for inp in tail.split(";")]
+        return Game(id, hands)
+
+    def ispossible(self, bag: Bag):
+        for hand in self.hands:
+            if not hand.ispossible(bag):
+                return False
+        return True
 
 
 def part1(inp: str):
-    return
+    bag = Bag(red=12, green=13, blue=14)
+    sum = 0
+    for line in inp.splitlines():
+        game = Game.parse(line)
+        if game.ispossible(bag):
+            sum += game.id
+    return sum
 
 
 # ---------------------------------------------------------------------------- #
