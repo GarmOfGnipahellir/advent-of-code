0e924fb7b538da216bc407d597a1993c8717be03
Garm <melsom.henrik@gmail.com>
2023-12-07 01:08:39 +0100

    2023 add rust template

---
+++ 2023/_template/src/main.rs
@@ -0,0 +1,31 @@
+const INP: &str = include_str!("../in.txt");
+
+fn part1(inp: &str) -> usize {
+    todo!()
+}
+
+fn part2(inp: &str) -> usize {
+    todo!()
+}
+
+fn main() {
+    println!("Part 1: {}", part1(INP));
+    println!("Part 2: {}", part2(INP));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EX1: &str = r#""#;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(EX1), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EX1), 0);
+    }
+}
